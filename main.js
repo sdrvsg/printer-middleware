8b722af68dbe2f88263572cf0430efaa9891cfba
Оникс <onyxservers@yandex.ru>
2024-05-03 02:25:47 +0400

    First

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, Tray, Menu, nativeImage } = require('electron')
+const { updateElectronApp } = require('update-electron-app');
+const axios = require('axios')
+const escpos = require('escpos')
+escpos.USB = require('escpos-usb')
+require('dotenv').config()
+updateElectronApp();
+
+const AppSecret = process.env.APP_SECRET;
+const poll = (device, printer) => {
+    axios.get('https://fikspizza.ru/api/receipts', {
+        headers: {
+            'X-App-Secret': AppSecret
+        }
+    }).then(
+        (response) => {
+
+            response.data?.orders?.forEach(order => {
+                console.info(order);
+            })
+
+            device?.open(function (error){
+
+                response.data?.orders?.forEach(order => {
+                    printer.text(order)
+                    this.cut()
+                })
+
+                this.close()
+
+            });
+
+        },
+        (error) => console.error(error.code)
+    )
+}
+
+app.whenReady().then(() => {
+
+    const icon = nativeImage.createFromPath('assets/img/icon.png')
+    const tray = new Tray(icon)
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Выход', type: 'normal', role: 'quit' },
+    ])
+
+    tray.setContextMenu(contextMenu)
+    tray.setToolTip('Fix Pizza Printer')
+
+    // const device  = new escpos.USB(0x76c, 0x0302)
+    // const printer = new escpos.Printer(device, {encoding: 'utf8'})
+
+    setInterval(poll/*.bind(device, printer)*/, 1000)
+
+})
